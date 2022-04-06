96d9121233c0ba96cd7f4b28d7d7f2a8d85842a7
Michael Min <michaelmin2020@gmail.com>
2022-04-06 14:02:40 -0500

    added counter component

---
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<templated>
+  <h3>{{ counter }}</h3>
+</templated>
+
+<script>
+export default {
+    computed: {
+        counter() {
+            return this.$store.state.counter;
+        },
+    },
+};
+</script>
