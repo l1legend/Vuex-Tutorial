6a3e29ad9af6ba2a7e8c5eff146406c52aceb2ad
Michael Min <michaelmin2020@gmail.com>
2022-04-06 17:12:40 -0500

    added more getter functions

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -19,7 +19,17 @@ const store = createStore({
   },
   getters: {
       finalCounter(state) {
-          return state.counter * 2;
+          return state.counter * 3;
+      },
+      normalizedCounter(_, getters) {
+        const finalCounter = getters.finalCounter;
+        if (finalCounter < 0) {
+            return 0;
+        }
+        if (finalCounter > 100) {
+            return 100;
+        }
+        return finalCounter;
       }
   }
 });
