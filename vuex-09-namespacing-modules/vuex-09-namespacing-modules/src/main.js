272954802310d0cc4f38d5e599a52aa009891100
Michael Min <michaelmin2020@gmail.com>
2022-04-07 16:03:32 -0500

    working version

--- vuex-09-namespacing-modules/vuex-09-namespacing-modules/src/main.js
+++ vuex-09-namespacing-modules/vuex-09-namespacing-modules/src/main.js
@@ -4,6 +4,7 @@ import { createStore } from 'vuex';
 import App from './App.vue';
 
 const counterModule = {
+  namespaced: true,
   state() {
     return {
       counter: 0
@@ -14,6 +15,7 @@ const counterModule = {
       state.counter = state.counter + 2;
     },
     increase(state, payload) {
+      console.log(state);
       state.counter = state.counter + payload.value;
     }
   },
@@ -26,9 +28,13 @@ const counterModule = {
     increase(context, payload) {
       console.log(context);
       context.commit('increase', payload);
-    }
+    },
+    login() {}
   },
   getters: {
+    testAuth(state) {
+      return state.isLoggedIn;
+    },
     finalCounter(state) {
       return state.counter * 3;
     },
@@ -79,6 +85,3 @@ const app = createApp(App);
 app.use(store);
 
 app.mount('#app');
-
-
-//.state is only used in the data store
